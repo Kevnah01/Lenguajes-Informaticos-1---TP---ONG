cbfc2a6b0651f9c9b98ad5542e7c901505e9fa5f
Nico MR <nicolas.medinarussomanno@estudiantes.unahur.edu.ar>
2026-06-28 23:29:01 -0300

    feat: 1.Coloque un nuevo boton con hover en contacto.html, index.html, nosotros.html, programas.html. 2.Al colocar un nuevo boton con nuevas funciones tuve que agregar cosas en main.js. 3.Achique algunas palabras de los navs.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 const botonModoOscuro = document.getElementById("btn-modo-oscuro");
+
 botonModoOscuro.addEventListener("click", () => {
     document.body.classList.toggle("modo-oscuro");
 
-    const modoActivo = document.body.classList.contains("modo-oscuro");
-    botonModoOscuro.textContent = modoActivo ? "Modo claro" : "Modo oscuro";
+    const currentTheme = document.documentElement.getAttribute("data-bs-theme");
+    const nextTheme = currentTheme === "dark" ? "light" : "dark";
+    document.documentElement.setAttribute("data-bs-theme", nextTheme);
+    
+    localStorage.setItem("theme", nextTheme);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const savedTheme = localStorage.getItem("theme") || "light";
+    
+    document.documentElement.setAttribute("data-bs-theme", savedTheme);
+    
+    if (savedTheme === "dark") {
+        document.body.classList.add("modo-oscuro");
+    } else {
+        document.body.classList.remove("modo-oscuro");
+    }
 });
